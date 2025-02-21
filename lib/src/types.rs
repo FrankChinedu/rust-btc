4d5acf3d55b001db2fe31aed9842f9e466dfe5e8
Obi Chinedu Frank <frankieetchy@gmail.com>
2025-02-21 21:46:32 +0100

    refactor transaction

--- lib/src/types.rs
+++ lib/src/types.rs
@@ -22,41 +22,6 @@ pub struct Blockchain {
     mempool: Vec<(DateTime<Utc>, Transaction)>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct Transaction {
-    pub inputs: Vec<TransactionInput>,
-    pub outputs: Vec<TransactionOutput>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct TransactionInput {
-    pub prev_transaction_output_hash: Hash,
-    pub signature: Signature,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct TransactionOutput {
-    pub value: u64,
-    pub unique_id: Uuid,
-    pub pubkey: PublicKey,
-}
-
-impl TransactionOutput {
-    pub fn hash(&self) -> Hash {
-        Hash::hash(self)
-    }
-}
-
-impl Transaction {
-    pub fn new(inputs: Vec<TransactionInput>, outputs: Vec<TransactionOutput>) -> Self {
-        Transaction { inputs, outputs }
-    }
-
-    pub fn hash(&self) -> Hash {
-        Hash::hash(self)
-    }
-}
-
 #[allow(clippy::new_without_default)]
 impl Blockchain {
     // utxos
