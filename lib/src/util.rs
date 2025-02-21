ec649972cd9a9fadc070473beed48d89d1add47a
Obi Chinedu Frank <frankieetchy@gmail.com>
2025-02-21 22:02:58 +0100

    add saveable triat

--- lib/src/util.rs
+++ lib/src/util.rs
@@ -1,7 +1,27 @@
-use serde::{Deserialize, Serialize};
-
 use crate::sha256::Hash;
 use crate::types::Transaction;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{Read, Result as IoResult, Write};
+use std::path::Path;
+
+pub trait Saveable
+where
+    Self: Sized,
+{
+    fn load<I: Read>(&self, reader: I);
+    fn save<O: Write>(&self, writer: O);
+    fn save_to_file<P: AsRef<Path>>(&self, path: P) -> IoResult<()> {
+        let file = File::create(&path)?;
+        self.save(file);
+        Ok(())
+    }
+    fn load_from_file<P: AsRef<Path>>(self, path: P) -> IoResult<Self> {
+        let file = File::open(&path)?;
+        self.load(file);
+        Ok(self)
+    }
+}
 
 #[derive(Deserialize, Serialize, Clone, Copy, Debug, PartialEq, Eq)]
 pub struct MerkleRoot(Hash);
