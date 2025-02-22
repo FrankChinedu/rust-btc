831dcdc2ff6235d3c1dab9fc7221a7c77625b263
Obi Chinedu Frank <frankieetchy@gmail.com>
2025-02-23 00:25:36 +0100

    wip

--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,40 +1,28 @@
+use anyhow::{anyhow, Result};
 use btclib::crypto::PublicKey;
 use btclib::network::Message;
 use btclib::types::Block;
 use btclib::util::Saveable;
+use clap::Parser;
 use std::env;
 use std::process::exit;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use std::thread;
 use tokio::net::TcpStream;
+use tokio::sync::Mutex;
+use tokio::time::{interval, Duration};
 
 #[tokio::main]
-async fn main() {
-    let address = match env::args().nth(1) {
-        Some(address) => address,
-        None => usage(),
-    };
-    let public_key_file = match env::args().nth(2) {
-        Some(public_key_file) => public_key_file,
-        None => usage(),
-    };
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let public_key = PublicKey::load_from_file(&cli.public_key_file)
+        .map_err(|e| anyhow!("Error reading public key: {}", e))?;
+    let miner = Miner::new(cli.address, public_key).await?;
 
-    let Ok(public_key) = PublicKey::load_from_file(&public_key_file) else {
-        eprintln!("Error reading public key from file {}", public_key_file);
-        exit(1);
-    };
-
-    println!("Connecting to {address} to mine with {public_key:?}",);
-
-    let mut stream = match TcpStream::connect(&address).await {
-        Ok(stream) => stream,
-        Err(e) => {
-            eprintln!("Failed to connect to server: {}", e);
-            exit(1);
-        }
-    };
-
-    println!("requesting work from {address}");
-    let message = Message::FetchTemplate(public_key);
-    message.send(&mut stream);
+    miner.run().await
 }
 
 fn usage() -> ! {
@@ -43,4 +31,76 @@ fn usage() -> ! {
         env::args().next().unwrap()
     );
     exit(1);
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    address: String,
+    #[arg(short, long)]
+    public_key_file: String,
+}
+
+#[allow(dead_code)]
+struct Miner {
+    public_key: PublicKey,
+    stream: Mutex<TcpStream>,
+    current_template: Arc<std::sync::Mutex<Option<Block>>>,
+    mining: Arc<AtomicBool>,
+    mined_block_sender: flume::Sender<Block>,
+    mined_block_receiver: flume::Receiver<Block>,
+}
+
+#[allow(dead_code)]
+impl Miner {
+    #[allow(unused_variables)]
+    async fn new(address: String, public_key: PublicKey) -> Result<Self> {
+        let stream = TcpStream::connect(&address).await?;
+        let (mined_block_sender, mined_block_receiver) = flume::unbounded();
+        Ok(Self {
+            public_key,
+            stream: Mutex::new(stream),
+            current_template: Arc::new(std::sync::Mutex::new(None)),
+            mining: Arc::new(AtomicBool::new(false)),
+            mined_block_sender,
+            mined_block_receiver,
+        })
+    }
+
+    async fn run(&self) -> Result<()> {
+        self.spawn_mining_thread();
+        let mut template_interval = interval(Duration::from_secs(5));
+        loop {
+            let receiver_clone = self.mined_block_receiver.clone();
+            tokio::select! {
+                _ = template_interval.tick() => {
+                    self.fetch_and_validate_template().await?;
+                }
+                Ok(mined_block) = receiver_clone.recv_async() => {
+                    self.submit_block(mined_block).await?;
+                }
+            }
+        }
+    }
+
+    fn spawn_mining_thread(&self) -> thread::JoinHandle<()> {
+        todo!()
+    }
+
+    async fn fetch_and_validate_template(&self) -> Result<()> {
+        todo!()
+    }
+
+    async fn fetch_template(&self) -> Result<()> {
+        todo!()
+    }
+
+    async fn validate_template(&self) -> Result<()> {
+        todo!()
+    }
+
+    async fn submit_block(&self, _block: Block) -> Result<()> {
+        todo!()
+    }
 }
